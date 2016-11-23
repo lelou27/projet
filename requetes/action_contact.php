30b83a3d603dfdedf8d6f95dbe416cbd94d2bd7c
Gautier <gautier.dujarrier@gmail.com>
2016-11-23 15:51:37 +0100

    Ajout de ajax.php et du dossier requetes avec les requetes

---
+++ requetes/action_contact.php
@@ -0,0 +1,58 @@
+<?php
+
+$errors = array();
+$success = false;
+
+if(!empty($_POST['Envoyer'])) {
+
+  $fullname = secu($_POST['fullname']);
+  $email = secu($_POST['email']);
+  $content = secu($_POST['content']);
+  $isAjax = secu($_POST['isAjax']);
+
+  $verif = verif($fullname, 5, 100, 'Votre nom');
+  if(!empty($verif)) { $errors['nickname'] = $verif; }
+
+  $verif = verif($email, 5, 120, 'Votre email');
+  if(!empty($verif)) { $errors['email'] = $verif; }
+
+  $verif = verif($content, 10, 100000, 'Votre message');
+  if(!empty($verif)) { $errors['content'] = $verif; }
+
+  if(count($errors) == 0) {
+
+    $mail = new PHPMailer;
+
+    $mail->setFrom($email, $nickname);
+    $mail->addAdress('admin@contact.php');
+
+    $mail->Subject = 'Demande d\'informations';
+    $mail->Body = '<p>' . $content . '</p>';
+    $mail->AltBody = $content;
+
+    if(!$mail->send()) {
+      echo 'Message could not be sent.';
+      echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
+
+  }
+  
+}
+
+if($isAjax == true) {
+
+  $response = array(
+    'errors' => $errors,
+    'success' => $success
+  );
+
+  $json = json_encode($response);
+  die($json);
+
+} else {
+
+  redirect('index.php');
+
+}
